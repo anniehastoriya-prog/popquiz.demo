2abd2ebf1405c22c73bd26d8682818f19d8720fa
Annie <anniehastoriya@gmail.com>
2026-04-23 19:19:48 -0500

    formatting the question 2

--- index.js
+++ index.js
@@ -10,3 +10,24 @@ answerInput = prompt(` What sweet food do bees produce?
  [1] Honey
  [2] Sugarcane
 `);
+answer = Number(answerInput);
+if (Number.isNaN(answer)) {
+  alert(" Enter a number. Honey or Sugarcane");
+} else if (answer < 1 || answer > 2) {
+  alert("Please enter 1 or 2.");
+} else {
+  if (answer === 1) {
+    alert("Correct!");
+    score = score + 1;
+  } else {
+    alert("Incorrect.");
+  }
+}
+
+// Question 2. i want the question to ask " Which country is the largest producer of olive oil?" I want the user to choose between Spain, Russia, or India>
+answerInput = prompt(` Which country is the largest producer of olive oil?
+    [1] Russia 
+    [2] India
+    [3] Spain
+
+`);
