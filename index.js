6e4c6820f0def7632c3a8747b129191c61b2b39c
Annie <anniehastoriya@gmail.com>
2026-04-23 19:08:48 -0500

    creating first question with the answers.

---
+++ index.js
@@ -0,0 +1,12 @@
+let score = 0;
+
+let answerInput;
+let answer;
+
+// Question 1. all questions will be food related. I want the  question to ask "What sweet food do bees produce?"
+// and I want the mutiple choice answer to be honey. I want the user to choose between Honey or Sugarcane.
+
+answerInput = prompt(` What sweet food do bees produce?
+ [1] Honey
+ [2] Sugarcane
+`);
